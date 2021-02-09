f7038a7c0ce7a3e853755f0fcd34977831fbb3cc
MadalinCiobanu <ciobanumadalinflorin@gmail.com>
2021-02-09 10:17:44 +0200

    add home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import './App.css';
+import Home from './components/Home';
+import Footer from './components/layout/Footer';
+import Header from './components/layout/Header';
+import {BrowserRouter as Router, Route} from "react-router-dom"
+import bg from "./background3.png"
 
 function App() {
+
+  // const pageStyle = {
+  //   backgroundImage: `url(${bg})`,
+  //   backgroundRepeat: "no-repeat",
+  //   backgroundSize: "80% 80%",
+  //   marginLeft: "10%"
+  // }
+
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Home />
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
