c064632b615da84ed40113b3171f7adf29a46ee1
MadalinCiobanu <ciobanumadalinflorin@gmail.com>
2021-02-11 09:49:47 +0200

    adjust home page structure

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,28 +1,15 @@
 import React from "react";
-import bg from "../try.jpg";
+import HomeBot from "./HomeBot";
+import HomeMid from "./HomeMid"
+import HomeTop from "./HomeTop";
 
 export default function Home() {
-  const pageStyle = {
-    background: `url(${bg}) no-repeat center center`,
-    webkitBackgroundSize: "cover",
-    mozBackgroundSize: "cover",
-    oBackgroundSize: "cover",
-    backgroundSize: "cover",
-    height: "900px",
-    justifyContent: "center",
-    alignItems: "center",
-    display: "flex",
-  };
-
-  const textStyle = {
-    color: "#E8E8E8",
-    textShadow: "2px 2px 2px black",
-    fontSize: "80px",
-  };
 
   return (
-    <div style={pageStyle}>
-      <h1 style={textStyle}>Trade goods simple and easy</h1>
+    <div style = {{backgroundColor: "#2F4858"}}>
+      <HomeTop/>
+      <HomeMid/>
+      <HomeBot/>
     </div>
   );
 }
---
+++ src/components/HomeTop.js
@@ -0,0 +1,76 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+import bg from "../try.jpg";
+
+export default function HomeTop() {
+  const pageStyle = {
+    background: `url(${bg}) no-repeat center center`,
+    webkitBackgroundSize: "cover",
+    mozBackgroundSize: "cover",
+    oBackgroundSize: "cover",
+    backgroundSize: "cover",
+    height: "900px",
+    justifyContent: "center",
+    alignItems: "center",
+    display: "flex"
+  };
+
+  const bigText = {
+    color: "#E8E8E8",
+    textShadow: "2px 2px 2px black",
+    flexFlow: "column wrap",
+    fontSize: "80px"
+  }
+
+  const mediumText = {
+    color: "#E8E8E8",
+    textShadow: "2px 2px 2px black",
+    flexFlow: "column wrap",
+    fontSize: "50px"
+  }
+
+  const buttonStyle = {
+      width: "8em",
+      height: "3em",
+      border: "1px solid #2F4858",
+      borderRadius: "8%",
+      color: "#2F4858"
+  }
+
+  function onHover (e) {
+    e.target.style.border = "2px solid white";
+    e.target.style.color = "white";
+    e.target.style.backgroundColor = "#2F4858";
+  }
+
+  function hoverOut (e) {
+    e.target.style.border = "2px solid #2F4858";
+    e.target.style.color = "#2F4858";
+    e.target.style.backgroundColor = "white";
+  }
+
+  const history = useHistory();
+
+  const routeChange = () => { 
+    let path = "/register"; 
+    history.push(path);
+  }
+
+  return (
+    <div style={pageStyle}>
+      <div>
+        <h1 style={bigText}>Do you have goods for sale?</h1>
+        <br/>
+        <p style={mediumText}>Join our community</p>
+        <br/>
+        <button
+            style={buttonStyle}
+            onMouseEnter = {onHover}
+            onMouseLeave = {hoverOut}
+            onClick = {routeChange}>
+                Register
+        </button>
+      </div>
+    </div>
+  );
+}
